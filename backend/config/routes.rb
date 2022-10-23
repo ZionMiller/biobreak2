5d762aeddbcddd767e2278d1bf50fc916019bb08
professorjrod <73684895+professorjrod@users.noreply.github.com>
2022-10-23 12:03:23 -0700

    added /me for profile

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: %i[create show]
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
+  get '/me', to: 'users#show'
   get '/logout', to: 'sessions#destroy'
   get 'logged_in', to: 'sessions#logged_in?'
 end
