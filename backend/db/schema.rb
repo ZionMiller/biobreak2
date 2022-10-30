bd6f5700e9b0c316d4ea1fe4767f59e7ad9c935f
professorjrod <73684895+professorjrod@users.noreply.github.com>
2022-10-30 15:56:07 -0700

    Fixing routes

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_22_003038) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_195000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "name"
+    t.string "ticker"
+    t.string "country"
+    t.string "catalyst"
+    t.string "biobreak_score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
